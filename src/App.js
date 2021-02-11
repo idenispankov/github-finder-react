ae8c4d7ca207974caabd64bd5efda73705d8af54
Denis Pankov <idenispankov@gmail.com>
2021-02-11 14:14:18 -0800

    api request for users added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
 import Search from './components/users/Search';
+import PropTypes from 'prop-types';
 
 class App extends Component {
 
@@ -12,15 +13,19 @@ class App extends Component {
     loading: false
   }
 
-  async componentDidMount() {
+  static propTypes ={
+    searchUsers: PropTypes.func.isRequired
+  }
+
+  searchUsers = async (text) => {
 
     this.setState({ loading: true });
 
-    const res = await axios.get(`https://api.github.com/users?
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&
     client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
     client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-    this.setState({ users: res.data, loading: false });
+    this.setState({ users: res.data.items, loading: false });
   }
 
   render() {
@@ -28,7 +33,7 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className="container">
-          <Search />
+          <Search searchUsers = {this.searchUsers} />
         <Users loading = {this.state.loading} users = {this.state.users}/>
         </div>
       </div>
