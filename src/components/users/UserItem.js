5e08f98070eab8f495dff0e17cc9f3d4ba5ea17d
Denis Pankov <idenispankov@gmail.com>
2021-02-11 17:21:08 -0800

    getting data from api about single user

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function UserItem({ user: { login, avatar_url, html_url } }) {
 
@@ -13,9 +14,9 @@ function UserItem({ user: { login, avatar_url, html_url } }) {
           />
           <h3>{login}</h3>
           <div>
-            <a href={html_url} className="btn btn-dark btn-sm my-1">
+            <Link to = {`/user/${login}`} className="btn btn-dark btn-sm my-1">
               More
-            </a>
+            </Link>
           </div>
         </div>
     )
@@ -25,4 +26,4 @@ UserItem.propTypes = {
   user: PropTypes.object.isRequired
 }
 
-export default UserItem
+export default UserItem;
