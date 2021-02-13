e8c4fe6b2d8bf6ec5768e4265052a1a35bc50fec
Denis Pankov <idenispankov@gmail.com>
2021-02-13 15:14:55 -0800

    Searc class to function, isung useState - learning to convert classes to function

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,46 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class Search extends Component {
+const Search = ({ searchUsers, showClear, clearUsers, setAlert }) =>  {
+  
+  const [text, setText] = useState('');
 
-  state = {
-    text: ''
-  };
-
-  static propTypes ={
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired
+  const onChange = (e) => {
+    setText(e.target.value);
   }
 
-  onChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
-
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    if (this.state.text === '') {
-      this.props.setAlert('Please, enter something', 'light')
+    if (text === '') {
+      setAlert('Please, enter something', 'light')
     } else {
-      this.props.searchUsers(this.state.text);
-      this.setState({ text: '' })
+      searchUsers(text);
+      setText('');
     }
   }
 
-  render() {
-
-    const {showClear, clearUsers } = this.props;
-
     return (
       <div>
-        <form onSubmit = {this.onSubmit} className='form'>
+        <form onSubmit = {onSubmit} className='form'>
           <input 
             type="text" 
             name='text' 
             placeholder='Search Users...'
-            value = {this.state.text}
-            onChange = {this.onChange}
+            value = {text}
+            onChange = {onChange}
           />
           <input 
             type="submit" 
@@ -56,6 +43,12 @@ class Search extends Component {
       </div>
     )
   }
+
+Search.propTypes ={
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired
 }
 
 export default Search
