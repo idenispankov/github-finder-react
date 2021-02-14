bac5f87df86b01b5170240d78b6a3c4266f64cbc
Denis Pankov <idenispankov@gmail.com>
2021-02-13 18:15:33 -0800

    NotFound component created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import Home from './components/pages/Home';
 import About from './components/pages/About';
 import User from './components/users/User';
+import NotFound from './components/pages/NotFound';
+
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 
@@ -21,21 +24,10 @@ const App = () =>  {
           <div className="container">
             <Alert />
             <Switch>
-              <Route exact path = '/' render = { props => (
-                <Fragment>
-                  <Search />
-                  <Users />
-                </Fragment>
-              )} 
-              />
-              <Route 
-                exact path = '/about' 
-                component = { About } 
-                />
-              <Route 
-                exact path = '/user/:login' 
-                component = {User}
-                />
+              <Route exact path = '/' component = { Home } />
+              <Route exact path = '/about' component = { About } />
+              <Route exact path = '/user/:login' component = {User} />
+              <Route component = { NotFound } />
             </Switch>
           </div>
         </div>
