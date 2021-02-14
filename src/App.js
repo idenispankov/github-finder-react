2a80ba803430569bbbe691b5e4bf393ace4bc3a9
Denis Pankov <idenispankov@gmail.com>
2021-02-13 17:01:27 -0800

    created Reducer & Action

--- src/App.js
+++ src/App.js
@@ -18,19 +18,6 @@ const App = () =>  {
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
-  // Search Users
-  const searchUsers = async (text) => {
-
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&
-    client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
-    client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    setUsers(res.data.items);
-    setLoading(false);
-  }
-
   // Get A Single GitHub User
   const getUser = async (username) => {
 
@@ -82,7 +69,6 @@ const App = () =>  {
               <Route exact path = '/' render = { props => (
                 <Fragment>
                   <Search 
-                    searchUsers = {searchUsers} 
                     clearUsers = {clearUsers} 
                     showClear = { users.length > 0 ?  true : false}
                     setAlert = {showAlert}
---
+++ src/context/github/githubReduser.js
@@ -0,0 +1,28 @@
+import {
+  SET_LOADING,
+  SEARCH_USERS,
+  CLEAR_USERS,
+  GET_USER,
+  GET_REPOS
+} from '../types';
+
+export default (state, action) => {
+  switch(action.type) {
+
+    case SEARCH_USERS:
+      return {
+        ...state,
+        users: action.payload,
+        loading: false
+      }
+
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true
+      }
+      
+    default:
+      return state;
+  }
+}
